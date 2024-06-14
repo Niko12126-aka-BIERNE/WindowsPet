c9ccb699329870a69ca3eaf88c84ac0bb277882c
Niko12126-aka-BIERNE <64725654+Niko12126-aka-BIERNE@users.noreply.github.com>
2024-06-14 17:37:38 +0200

    Adding WindowManager and MouseManager

---
+++ WindowsPet/MouseManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WindowsPet
+{
+    internal class MouseManager
+    {
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out Point lpPoint);
+
+        public static Point GetMouseLocation()
+        {
+            GetCursorPos(out Point lpPoint);
+
+            return lpPoint;
+        }
+    }
+}
---
+++ WindowsPet/WindowManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WindowsPet
+{
+    internal partial class WindowManager
+    {
+        [LibraryImport("user32.dll")]
+        private static partial IntPtr GetForegroundWindow();
+
+        [LibraryImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [LibraryImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool IsWindowVisible(IntPtr hWnd);
+
+        [LibraryImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool IsIconic(IntPtr hWnd); // Check if window is minimized
+
+        [LibraryImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool IsZoomed(IntPtr hWnd); // Check if window is maximized
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        public static RECT? GetFocusedWindowRectangle(IntPtr petHandle, IntPtr homeHandle)
+        {
+            IntPtr focusedWindowPtr = GetForegroundWindow();
+
+            if (focusedWindowPtr == petHandle || focusedWindowPtr == homeHandle)
+            {
+                return null;
+            }
+
+            if (IsZoomed(focusedWindowPtr))
+            {
+                return null;
+            }
+
+            if (!IsWindowVisible(focusedWindowPtr) || IsIconic(focusedWindowPtr))
+            {
+                return null;
+            }
+
+            return GetWindowRectangle(focusedWindowPtr);
+        }
+
+        public static RECT GetWindowRectangle(IntPtr windowHandle)
+        {
+            GetWindowRect(windowHandle, out RECT focusedWindowRectangle);
+            return focusedWindowRectangle;
+        }
+    }
+}
