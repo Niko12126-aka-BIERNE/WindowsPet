29ea4e1a9e33c704c268856d7491b7c57d9bbffa
Niko12126-aka-BIERNE <64725654+Niko12126-aka-BIERNE@users.noreply.github.com>
2024-06-14 17:39:08 +0200

    Adding HomeForm

---
+++ WindowsPet/HomeForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WindowsPet
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            pictureBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox.Location = new Point(0, 0);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(800, 450);
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            pictureBox.MouseDown += HomeMouseDown;
+            pictureBox.MouseMove += HomeMouseMove;
+            pictureBox.MouseUp += HomeMouseUp;
+            // 
+            // HomeForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "HomeForm";
+            Text = "HomeForm";
+            TopMost = true;
+            TransparencyKey = SystemColors.Control;
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox;
+    }
+}
---
+++ WindowsPet/HomeForm.cs
@@ -0,0 +1,56 @@
+﻿namespace WindowsPet
+{
+    public partial class HomeForm : Form
+    {
+        public Point CurrentLocation { get; private set; }
+        private bool mouseDown;
+        private Point lastLocation;
+        public static bool StayAtHome { get; private set; }
+
+        public HomeForm(Bitmap homeSprite, Pet windowsPet)
+        {
+            CurrentLocation = new Point(Location.X + homeSprite.Width / 2, Location.Y + homeSprite.Height);
+            mouseDown = false;
+            StayAtHome = false;
+
+            InitializeComponent();
+
+            pictureBox.Image = homeSprite;
+
+            IntPtr handle = Handle;
+            new Thread(() => Application.Run(new PetForm(windowsPet, handle))).Start();
+        }
+
+        private void HomeMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDown = true;
+                lastLocation = e.Location;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                if (!Size.Equals(pictureBox.Image.Size))
+                {
+                    Size = pictureBox.Image.Size;
+                }
+
+                StayAtHome = !StayAtHome;
+            }
+        }
+
+        private void HomeMouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                Location = new Point((Location.X - lastLocation.X) + e.X, (Location.Y - lastLocation.Y) + e.Y);
+                CurrentLocation = new Point(Location.X + pictureBox.Image.Width / 2, Location.Y + pictureBox.Image.Height);
+            }
+        }
+
+        private void HomeMouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+    }
+}
